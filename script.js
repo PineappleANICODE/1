d847371aaf1cde5a6333c5566e1886589d4d3ed6
PineappleANICODE <118062553+PineappleANICODE@users.noreply.github.com>
2024-02-15 11:25:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ el.addEventListener("click", formatName, false);
 function formatName() {
   var NAME = document.getElementById('NAME').value;
 
-
-  document.getElementById('fullName').innerHTML = NAME+ ", YOU DON'T EXIST. "+NAME+ ", YOU DON'T EXIST. "+NAME+ ", YOU DON'T EXIST. "+NAME+ ", YOU DON'T EXIST";
+  
+  if (NAME === "searcher") {
+    text = "Not yet...";
+    
+  
+  } else if (NAME === "Searcher") {
+    text = "Not yet...";
+    
+    } else if (NAME === "God") {
+    text = "Really? I don't believe you. Prove it <link>";
+      
+       } else if (NAME === "god") {
+    text = "Not even capitalised!! The real God would be more careful";
+    
+  
+  } else {
+      document.getElementById('fullName').innerHTML = NAME+ ", YOU DON'T EXIST. "+NAME+ ", YOU DON'T EXIST. "+NAME+ ", YOU DON'T EXIST. "+NAME+ ", YOU DON'T EXIST";
+  }
 }
 
 
